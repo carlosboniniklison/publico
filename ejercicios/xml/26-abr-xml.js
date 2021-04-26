349ae0fb2781b120299a40d26dc2633053474f7c
Prof. Carlos Boni <carlosboni.niklison@gmail.com>
2021-04-26 20:47:44 +0200

    xml

---
+++ ejercicios/xml/26-abr-xml.js
@@ -0,0 +1,32 @@
+/* 
+JavaScript / jQuery
+web o pagina:
+autor:
+fecha:
+resumen:
+*/
+function leerXML() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      miFuncion(this);
+    }
+  };
+  xhr.open("GET", "registrados.xml", true);
+  xhr.send();
+}
+
+function miFuncion(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var tabla="<tr><th>Empleado</th><th>Clave</th></tr>";
+  var x = xmlDoc.getElementsByTagName("usuario");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("contenidoXML").innerHTML = tabla;
+}
