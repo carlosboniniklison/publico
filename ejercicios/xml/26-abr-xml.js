d502cdefc87fffa400207aaa96e18d9b5cbb19fd
Prof. Carlos Boni <carlosboni.niklison@gmail.com>
2021-04-27 11:18:41 +0200

    XML final

--- ejercicios/xml/26-abr-xml.js
+++ ejercicios/xml/26-abr-xml.js
@@ -6,21 +6,59 @@ fecha:
 resumen:
 */
 function leerXML() {
+  // lee desde GitHub.
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      miFuncion(this);
+      miFuncion(this,"Desde GitHub");
     }
   };
   xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
   xhr.send();
 }
 
-function miFuncion(xml) {
+function leerXML0() {
+  // lee desde GitHub.
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      miFuncion(this,"Desde GitHub con atributos");
+    }
+  };
+  xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados2.xml", true);
+  xhr.send();
+}
+
+function leerXML1() {
+  // lee desde Axartec.
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      miFuncion(this,"Desde Axartec");
+    }
+  };
+  xhr.open("GET", "https://appslab.axartec.com/pruebas/registrados.xml", true);
+  xhr.send();
+}
+
+function leerXML2() {
+  // lee desde aquí.
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      miFuncion(this,"Desde aquí");
+    }
+  };
+  xhr.open("GET", "registrados.xml", true);
+  xhr.send();
+}
+
+function miFuncion(xml,fuente) {
   var i;
   var xmlDoc = xml.responseXML;
-  var tabla="<table><tr><th>Empleado</th><th>Clave</th></tr>";
+  var tabla=fuente + "<br/>";
   var x = xmlDoc.getElementsByTagName("usuario");
+  tabla += "<table><tr><th>Empleado</th><th>Clave</th></tr>";
   for (i = 0; i <x.length; i++) { 
     tabla += "<tr><td>" +
     x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue +
